d87089725a4eae5c3527acba36759b422aed120b
ozgeterz <terziozge@hotmail.com>
2026-02-27 23:59:33 +0300

    fixes

--- src/pages/SheetHolder.js
+++ src/pages/SheetHolder.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { turkeyData } from '../data/turkeyData';
 import './ProductPage.css';
 import img1 from '../assets/ÇARŞAFLARI KİLİTLE.png';
